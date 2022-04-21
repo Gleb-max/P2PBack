289c496da0ffe4c78b3cf64011a7df6b2ed6dc4d
Gleb <gleb.demin.04@mail.ru>
2022-04-21 21:22:07 +0300

    enable cors

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, abort
+from flask_cors import CORS, cross_origin
 import json
 
 import utils
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/tx/<transaction>')
+@cross_origin()
 def search_transaction(transaction):
     search_result = utils.api_search(transaction)
     if not search_result:
